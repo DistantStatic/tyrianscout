7c8d28956745f95ce67ef194e25f5f8347535de1
Oscar Rodriguez <oscarrodriguez101@gmail.com>
2021-10-22 03:38:28 -0400

    daily modal and item display fleshed out

--- src/components/item/item.js
+++ src/components/item/item.js
@@ -1,10 +1,12 @@
 import { Figure, Image } from "react-bootstrap";
+import styles from './item.module.css';
 
-export default function ItemDisplay(item){
-
+export default function ItemDisplay({item}){
+    console.log(item);
     return (
-        <Figure>
-            <h3> {item.name} </h3>
+        <Figure className={styles.item}>
+            <h4> <u>Reward</u> </h4>
+            <h5> {item.name} </h5>
             <Image rounded src={item.icon}/>
             <p><i>{item.description}</i></p>
             <p>Chat Link: {item.chat_link}</p>
--- src/modals/daily/daily.js
+++ src/modals/daily/daily.js
@@ -1,38 +1,34 @@
 import { useState, useEffect } from 'react';
 
-import { Modal, Button, ListGroup } from 'react-bootstrap';
-import Item from '../../components/item/item';
+import { Modal, Button, ListGroup, Col, Row } from 'react-bootstrap';
+import styles from './daily.module.css';
 
+import Item from '../../components/item/item';
 import axios from '../../axios-instances/axios-gw2';
 
 export default function DailyModal(props){
-    console.log(props.daily);
     const [reward, setReward] = useState({});
 
     useEffect(() => {
-        if (!props.show) return;
+        if (typeof(props.daily.rewards) === 'undefined') return setReward(null);
+        if (typeof(props.daily.rewards[0]) === 'undefined') return setReward(null);
+        //set loading to hide reward while loading
         axios({
             method: 'GET',
             url:`/items/${props.daily.rewards[0]['id']}`
         })
         .then(response => {
-            setReward(response.data)
+            setReward(response.data);
         })
-    }, [])
+    }, [props])
 
     return(
         <Modal show={props.show} onHide={props.hide}>
             <Modal.Header>
-                {
-                    //Daily Name
-                }
                 <Modal.Title>{props.daily.name}</Modal.Title>
                 <Button variant="danger" onClick={props.hide}>X</Button>
             </Modal.Header>
             <Modal.Body>
-                {
-                    //Daily details
-                }
                 <p>
                     {props.daily.requirement}
                 </p>
@@ -41,18 +37,23 @@ export default function DailyModal(props){
                 }
                 <ListGroup>
                     {
-                        props.daily.tiers.map(tier => {
-                            <ListGroup.Item>{tier.count} | {tier.points}</ListGroup.Item>
-                        })
+                        props.daily.tiers.map((tier, index) => (
+                            <ListGroup.Item>
+                                <Row className={styles.tierRow}>
+                                    <Col xs={4}>Count: {tier.count}</Col> <Col xs={4}>{`Tier: ${index + 1}`} </Col> <Col xs={4}>Points: {tier.points}</Col>
+                                </Row>
+                            </ListGroup.Item>
+                        ))
                     }
                 </ListGroup>
-            </Modal.Body>
-            <Modal.Footer>
                 {
-                    //Daily Rewards
+                    //TODO: Make loading neater by hiding rewardbox while loading
                 }
-                <Item item={reward} />
-            </Modal.Footer>
+                <hr />
+                <div className={styles.rewardBox} >
+                    { reward ? <Item item={reward} /> : "" }
+                </div>
+            </Modal.Body>
         </Modal>
     )
 }
